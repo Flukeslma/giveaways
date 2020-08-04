2794252fbc85b5e4fc4277c6a7a9dd2a4b2a65eb
Flukeslma <65383347+Flukeslma@users.noreply.github.com>
2020-08-04 18:33:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,9 @@
-const fs = require('fs');
+const Discord = require('discord.js");
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
 const colors = require('./colors.json')
 
 const config = require('./config.json')
-client.config = config;
-
-
-const { GiveawaysManager } = require('discord-giveaways');
-client.giveawaysManager = new GiveawaysManager(client, {
-    storage: "./giveaways.json",
-    updateCountdownEvery: 750,
-    default: {
-        botsCanWin: false,
-        exemptPermissions: [ "MANAGE_MESSAGES" ],
-        embedColor: "#00FF27",
-        reaction: "🎉"
-    }
-});
-
-client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
-    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
-});
-
-client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
-    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
-});
-
-fs.readdir("./events/", (_err, files) => {
-    files.forEach((file) => {
-        if (!file.endsWith(".js")) return;
-        const event = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        console.log(`👌 Event loaded: ${eventName}`);
-        client.on(eventName, event.bind(null, client));
-        delete require.cache[require.resolve(`./events/${file}`)];
-    });
-});
-
-client.commands = new Discord.Collection();
-
-/* Load all commands */
-fs.readdir("./commands/", (_err, files) => {
-    files.forEach((file) => {
-        if (!file.endsWith(".js")) return;
-        let props = require(`./commands/${file}`);
-        let commandName = file.split(".")[0];
-        client.commands.set(commandName, props);
-        console.log(`👌 Command loaded: ${commandName}`);
-    });
-});
 
 client.on('message', message =>{
     if (message.content === "/ping")
@@ -58,35 +11,14 @@ client.on('message', message =>{
 });
 
 client.on('ready', () => {
-    client.user.setActivity("Awaken On top!", {
+    client.user.setActivity("/nuke", {
         type: "PLAYING",
       });
     });
 
-    client.on('message', message => {
-        if (message.content === "/help") {
-      const embed = new Discord.MessageEmbed()
-         embed.setTitle("Commands Help")
-         embed.addFields(
-             { name: "Start", value: "usage: /start <Channel> <time> <winnercount> <Prize>. This commands starts a giveaway"},
-             { name: "reroll", value: "usage: /reroll <givaway-message-id>. This command Rerolls a ended giveaway"},
-             { name: "end", value: "usage: /end <giveaway-message-id>. This command ends a ongoing giveaway"},
-             { name: "Ping", value: "usage: /ping. This Command showsThe bot's ping. "},
-             { name: "Members", value: "esage: /members - Displays the current Member Count"})
-        embed.setFooter("Need more help? Contact flickz#0865!")
-        embed.setTimestamp()
-        embed.setColor(colors.red)
-        message.channel.send(embed);
-    } else if (message.content === "/members") {
-        const embed = new Discord.MessageEmbed()
-        embed.setTitle("Total Members:")
-        embed.setDescription(`Total Members: ${message.guild.memberCount}`)
-        embed.setTimestamp()
-        embed.setFooter("AwaKen On Top")
-        embed.setColor(colors.indigo)
-        message.channel.send(embed)
-      }
-    })
-    
+client.on('message', message => {
+      if (message.content === "/nuke")
+      message.channel.send("<@&739962712850694245>, this skid wants to nuke!")
+}
 
 client.login(config.token);
